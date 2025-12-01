7f77c6c2289199a536874a656777cbb0493e461b
luciakusumaw <itsmelucia27@gmail.com>
2025-12-01 09:26:02 +0700

    Question 4, no 3

--- ExpressingGratitude_13.java
+++ ExpressingGratitude_13.java
@@ -3,19 +3,21 @@ import java.util.Scanner;
 public class ExpressingGratitude_13 {
     public static String getGreetingRecipient() {
         Scanner input = new Scanner(System.in);
-        System.out.print("Input the name of people you want to greet : ");
+        System.out.print("Input the name of people you want to greet: ");
         String recipientName = input.nextLine();
         return recipientName;
     }
-
     public static void sayThankyou() {
         String name = getGreetingRecipient();
         System.out.println("Thank you " + name + " for being the best teacher in the world.\n" +
-                "You inspired in me a love for learning and made me feel like I could ask you anything.");
+                "You inspired me a love for learning and made me feel like I could ask you anything.");
     }
-
+    public static void sayAdditionalGreetings(String greeting) {
+        System.out.println(greeting);
+    }
     public static void main(String[] args) {
         sayThankyou();
+        sayAdditionalGreetings("Wish you all the best and thank you for your kindness!");
     }
 }
 
