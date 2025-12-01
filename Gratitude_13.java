e18a9fd5ff4cde6d068364af9c043ace2cb96b99
luciakusumaw <itsmelucia27@gmail.com>
2025-12-01 08:51:31 +0700

    Experiment 2

--- Gratitude_13.java
+++ Gratitude_13.java
@@ -1,5 +1,18 @@
 public class Gratitude_13 {
-    public static void main(String[] args) {
-        System.out.println("Thank you for being the best teacher in the world.\n" + "You inspired in me a love for learning and made me feel like I could ask you anything.");
-    }
+    public Gratitude_13() {
+   }
+
+   public static void sayThankyou() {
+    System.out.println("Thank you for being the best teacher in the world.\nYou inspired in me a love for learning and made me feel like I could ask you anything.");
+   }
+
+   public static void sayAdditionalGreetings(String greeting) {
+    System.out.println(greeting);
+   }
+
+   public static void main(String[] args) {
+    sayThankyou();
+    String expression = "Thankyou...wish you all the best!!";
+    sayAdditionalGreetings(expression);
+   }
 }
